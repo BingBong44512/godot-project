4b442998be6cd4780ac55442154941f1d56538b1
potato33s <burneee30@gmail.com>
2026-04-25 11:45:20 -0400

    Dread for the Bread

---
+++ scripts/game_logic.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var toads :=0
+var eagle:=0
+var owl:=0
+var bear:=0
+var wolf := 0
+var bison := 0
+var elk := 0
+var snake :=0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
