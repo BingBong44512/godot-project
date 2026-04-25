83203ad418eba8613e1570ad7262fdabb0ea6fb9
potato33s <burneee30@gmail.com>
2026-04-25 12:44:06 -0400

    Health System

--- scripts/game_logic.gd
+++ scripts/game_logic.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+const SUNLIGHT = 5
+
+const P1 := SUNLIGHT * 10 + 5
+const P2 := SUNLIGHT * 100 + 10
+
 var toads :=0
 var eagle:=0
 var owl:=0
@@ -8,6 +13,20 @@ var wolf := 0
 var bison := 0
 var elk := 0
 var snake :=0
+var grass :=0
+@export var health:=0:
+	get:
+		var energy = SUNLIGHT * grass
+		if P1 * (toads+elk+bison+snake) > energy:
+			energy -= P1 * (toads+elk+bison+snake)
+		else:
+			energy += P1 * (toads+elk+bison+snake)
+		if P2 * (eagle+owl+bear+wolf)> energy:
+			energy -= P2 * (eagle+owl+bear+wolf)
+		else:	
+			energy += P2 * (eagle+owl+bear+wolf)
+		return energy
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
